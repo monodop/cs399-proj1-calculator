a9a9b80832b7bea8b919d5c648c6cb98ff2d08c3
Harrison Lambeth <harrison@nau.edu>
2016-10-02 17:00:31 -0700

    Added calculator screen

---
+++ js/Calculator.js
@@ -0,0 +1,77 @@
+/**
+ * @providesModule
+ */
+
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons'
+
+export class Calculator extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Icon.ToolbarAndroid
+                    style={styles.toolbar}
+                    title="Clackulator"
+                    actions={[
+                        {
+                            title: 'Settings',
+                            show: 'always',
+                            iconName: 'md-settings'
+                        }
+                    ]}
+                />
+                <Text style={styles.welcome}>
+                    Welcome to React Native! Hello, I am harrison!
+                </Text>
+                <Text style={styles.instructions}>
+                    To get started, edit index.android.js
+                </Text>
+                <Text style={styles.instructions} onClick={this.onClick}>
+                    Double tap R on your keyboard to reload,{'\n'}
+                    Shake or press menu button for dev menu
+                </Text>
+                <Text style={styles.annoying}>
+                    Check out my sick calculator!
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        alignItems: 'stretch',
+        backgroundColor: '#F5FCFF',
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+    annoying: {
+        textAlign: 'right',
+        color: '#000000',
+        marginBottom: 5,
+        backgroundColor: '#ff0000',
+        padding: 100
+    },
+    toolbar: {
+        height: 50,
+        backgroundColor: '#e9eaed'
+    }
+});
+
+export default Calculator;
